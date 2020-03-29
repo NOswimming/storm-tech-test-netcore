dab180b56fd0759676abfda67098a94196df7e33
Callum Fitt-Simpson <callum.fitt.simpson@gmail.com>
2020-03-29 19:12:32 +0100

    Task 9: Get Gravatar Profile with service

--- Todo/EntityModelMappers/TodoItems/UserSummaryViewmodelFactory.cs
+++ Todo/EntityModelMappers/TodoItems/UserSummaryViewmodelFactory.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Todo.Models.TodoItems;
+using Todo.Services;
 
 namespace Todo.EntityModelMappers.TodoItems
 {
@@ -7,7 +8,16 @@ namespace Todo.EntityModelMappers.TodoItems
     {
         public static UserSummaryViewmodel Create(IdentityUser identityUser)
         {
-            return new UserSummaryViewmodel(identityUser.UserName, identityUser.Email);
+            var userSummaryViewmodel = new UserSummaryViewmodel(identityUser.UserName, identityUser.Email);
+
+            // Add gravatar profile information if available;
+            var gravatarProfile = GravatarProfileService.GetService().GetGravatarProfile(identityUser.Email);
+            if (gravatarProfile != null)
+            {
+                userSummaryViewmodel.FullName = gravatarProfile.FullName;
+            }
+
+            return userSummaryViewmodel;
         }
     }
 }
---
+++ Todo/Services/GravatarProfileService.cs
@@ -0,0 +1,64 @@
+﻿using GravatarSharp.Core;
+using GravatarSharp.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Todo.Services
+{
+    public class GravatarProfileService
+    {
+        private GravatarController gravatarController;
+        private Dictionary<string, GravatarProfile> gravatarProfileCache;
+
+        private static GravatarProfileService instance;
+
+        private GravatarProfileService()
+        {
+            gravatarController = new GravatarController();
+            gravatarProfileCache = new Dictionary<string, GravatarProfile>();
+        }
+
+        /// <summary>
+        /// Gets the singleton Gravatar Profile service for static use.
+        /// </summary>
+        /// <returns>Singleton Gravatar Profile service</returns>
+        public static GravatarProfileService GetService()
+        {
+            if (instance == null)
+            {
+                instance = new GravatarProfileService();
+            }
+
+            return instance;
+
+        }
+
+        public GravatarProfile GetGravatarProfile(string emailAddress)
+        {
+            // Try to get the cached value;
+            if (gravatarProfileCache.TryGetValue(emailAddress, out var cachedProfile))
+            {
+                return cachedProfile;
+            }
+
+            // TODO: Change this to use async tasks all the way through the call stack.
+            try
+            {
+                var gravatarProfileResult = gravatarController.GetProfile(emailAddress).Result;
+
+                if (gravatarProfileResult.Profile != null && gravatarProfileResult.ErrorMessage == null)
+                {
+                    gravatarProfileCache.Add(emailAddress, gravatarProfileResult.Profile);
+                    return gravatarProfileResult.Profile;
+                }
+            }
+            catch (Exception e)
+            {
+                // TODO: Handle Gravatar profile request failiures
+            }
+
+            return null;
+        }
+    }
+}
